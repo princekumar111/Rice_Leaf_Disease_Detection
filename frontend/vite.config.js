517bd24b3f895be4ba1522aa0d9e4bb53b5682e6
princekumar111 <princekumar060420@gmail.com>
2026-05-18 14:52:40 +0530

    Fixed Vite allowed host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,20 +3,21 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(
+    import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-    strictPort: false,
-    allowedHosts: "*",
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    server: {
+        host: "::",
+        port: 8080,
+        strictPort: false,
+        allowedHosts: ['paddy-guard.onrender.com'],
     },
-  },
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 });
