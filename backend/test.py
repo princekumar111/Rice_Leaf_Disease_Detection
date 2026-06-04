65591ad6aa383722286f3881afd4b2317ecd1adb
princekumar111 <princekumar060420@gmail.com>
2026-06-04 14:37:22 +0530

    update backend api url

--- backend/test.py
+++ backend/test.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://127.0.0.1:5000/predict"
+url = "https://rice-leaf-disease-detection-waco.onrender.com/predict"
 
 files = {
     "image": open("dataset\Brown_spot\Brown_spot (12).jpg", "rb")
