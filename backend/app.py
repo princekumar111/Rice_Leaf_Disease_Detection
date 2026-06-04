4a55ab7f56d614ed333dc474c880b49a2d415686
princekumar111 <princekumar060420@gmail.com>
2026-06-04 17:22:51 +0530

    fix flask api errors

--- backend/app.py
+++ backend/app.py
@@ -1,35 +1,55 @@
-from flask_cors import CORS
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from tensorflow.keras.models import load_model
 import numpy as np
 from PIL import Image
 
+
 app = Flask(__name__)
+CORS(app)
 
 
-
-    @app.route('/')
+@app.route('/')
 def home():
     return "Rice Leaf Disease API is running"
 
 
-CORS(app)
 model = load_model("rice_model.h5")
 
-class_names = ['Brown_spot','Bacterial_Blight','Healthy','Leaf_Blast']
+class_names = [
+    'Brown_spot',
+    'Bacterial_Blight',
+    'Healthy',
+    'Leaf_Blast'
+]
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    file = request.files['file']
-    img = Image.open(file).resize((224,224))
-    img = np.array(img)/255.0
-    img = np.expand_dims(img, axis=0)
+    try:
+        if 'file' not in request.files:
+            return jsonify({"error": "No image uploaded"}), 400
 
-    prediction = model.predict(img)
-    result = class_names[np.argmax(prediction)]
+        file = request.files['file']
 
-    return jsonify({"prediction": result})
+        img = Image.open(file).convert("RGB")
+        img = img.resize((224, 224))
+
+        img = np.array(img) / 255.0
+        img = np.expand_dims(img, axis=0)
+
+        prediction = model.predict(img)
+        result = class_names[np.argmax(prediction)]
+
+        return jsonify({
+            "prediction": result
+        })
+
+    except Exception as e:
+        return jsonify({
+            "error": str(e)
+        }), 500
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
